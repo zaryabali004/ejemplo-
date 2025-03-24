8dc59dacdaaafbd95149cbdc54520a9ff0628ba2
Zaryab Ali <zaryaba941@gmail.com>
2025-03-24 10:18:40 +0100

    Cambio desde rama master

--- Hola.java
+++ Hola.java
@@ -2,7 +2,7 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("¡Hola desde Git Bash!");
 		System.out.println("Nuevo cambio");
-		System.out.println("Esto lo escribí en Eclipse en la rama nueva");
+		System.out.println("Esto lo escribí en Eclipse en la rama master");
 
 	}
 }
