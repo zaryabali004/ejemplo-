271ee38c24db991d9c615f790679a2e432d56994
Zaryab Ali <zaryaba941@gmail.com>
2025-03-24 09:57:10 +0100

    nuevo mensaje corregido

--- Hola.java
+++ Hola.java
@@ -1,1 +1,6 @@
-public class Hola { public static void main(String[] args) { System.out.println("¡Hola desde Git Bash!"); } }
+public class Hola {
+	public static void main(String[] args) {
+		System.out.println("¡Hola desde Git Bash!");
+		System.out.println("Nuevo cambio");
+	}
+}
